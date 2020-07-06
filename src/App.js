d9cdfb7f058e425e06c9a76dbf31a3db975e668d
abdulsamigithub <asm640@gmail.com>
2020-07-06 17:13:06 +0500

    updated code dinner

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
  import React from 'react';
-
+ import './App.css'
+import Dinner from './dinner.js';
 function App() {
-  return <h1>hellow world from sami</h1>
+  return (
+    <div className='App'>
+  {/* <h1>hellow world from sami</h1> */}
+    <Dinner dishName="chicken Karahi"  sweetDish="gulaab jamun"/>
+    <Dinner dishName="chicken biryani"  sweetDish="kheer"/>
+    <Dinner dishName="chicken nihari"  sweetDish="halwa"/>
 
+
+    </div>
+  )
 }
 
 export default App;
---
+++ src/dinner.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Dinner (props){
+
+    return(
+        <div>
+        <h1>today we are serving mian dish as {props.dishName}</h1>
+    <h1> today we are also serving sweet dish of {props.sweetDish}</h1>
+        </div>
+    
+        )
+}
+export default Dinner;
